e1619d10d42434d2dcbf2eae93acc431c8ee07f0
Mereplain <152766240+Mereplain@users.noreply.github.com>
2025-04-05 00:50:34 +0800

    Delete SkyFit directory

--- SkyFit/scripts/openSettings.js
+++
@@ -1,7 +0,0 @@
-let btnOpenerSetting = document.querySelector('.header__button-settings');
-let windowSettings = document.querySelector('.header__window-settings.visually-hidden');
-
-btnOpenerSetting.addEventListener('click', function(){
-    windowSettings.classList.toggle('visually-hidden');
-})
-
--- SkyFit/scripts/switcherThem.js
+++
@@ -1,8 +0,0 @@
-let btnSwitcherThem = document.querySelector('.header__button-theme');
-let circleInBtn = document.querySelector('.header__setting-theme-circle')
-let body = document.querySelector('body');
-
-btnSwitcherThem.addEventListener('click', function(){
-    circleInBtn.classList.toggle('btnThemAnim');
-    body.classList.toggle('light-them');
-})
